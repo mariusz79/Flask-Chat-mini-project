b7b3b887a83ebc4b7f2023252fb3998cc9e27b8f
mariusz79 <mpawlowic@gmail.com>
2019-03-28 00:07:31 +0000

    Storing messages in a dict

--- run.py
+++ run.py
@@ -8,11 +8,8 @@ messages = []
 def add_messages(username, message):
     """Add messages to the messages' list"""
     now = datetime.now().strftime("%H:%M:%S")
-    messages.append("({}) {}: {}".format(now, username, message))
-
-def get_all_messages():
-    """Get all the messages and separate them with a 'br'"""
-    return "<br>".join(messages)
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
 
 @app.route('/', methods = ["GET", "POST"])             
 def index():
@@ -31,7 +28,7 @@ def index():
 @app.route('/<username>')
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1> {1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1> {1}".format(username, messages)
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
