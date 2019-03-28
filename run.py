d798b0780bc8aba32ce1629cd4ad210da90ca54e
mariusz79 <mpawlowic@gmail.com>
2019-03-28 00:57:41 +0000

    Creating chat textbox

--- run.py
+++ run.py
@@ -25,9 +25,16 @@ def index():
 
 
 """If we use angle brackets <> content inside gets treated as variable"""
-@app.route('/<username>')
+@app.route('/<username>', methods = ["GET", "POST"])
 def user(username):
     """Display chat messages"""
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
     return render_template("chat.html", username = username, chat_messages = messages)
 
 @app.route('/<username>/<message>')
