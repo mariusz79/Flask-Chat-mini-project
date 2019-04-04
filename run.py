d474954ee123f549c9e9bebd365c149ff7f4c887
mariusz79 <mpawlowic@gmail.com>
2019-04-04 13:49:11 +0100

    last commit

--- run.py
+++ run.py
@@ -1,47 +1,52 @@
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, session
 
+
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
+
 def add_messages(username, message):
-    """Add messages to the messages' list"""
+    """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages_dict = {"timestamp": now, "from": username, "message": message}
     messages.append(messages_dict)
 
-@app.route('/', methods = ["GET", "POST"])             
-def index():
-    """ Main page with instructions """
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Main page with instructions"""
     if request.method == "POST":
         session["username"] = request.form["username"]
 
     if "username" in session:
-        return redirect (session["username"])
+        return redirect(session["username"])
 
     return render_template("index.html")
 
 
-"""If we use angle brackets <> content inside gets treated as variable"""
-@app.route('/<username>', methods = ["GET", "POST"])
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
-
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
         add_messages(username, message)
         return redirect(session["username"])
 
-    return render_template("chat.html", username = username, chat_messages = messages)
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
-@app.route('/<username>/<message>')
+
+@app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
-    return redirect(username)
+    return redirect("/" + username)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
