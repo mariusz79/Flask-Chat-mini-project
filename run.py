99d20567d4401e4a526d4b78af9bb2788ee6292c
mariusz79 <mpawlowic@gmail.com>
2019-04-04 22:00:14 +0100

    commit

--- run.py
+++ run.py
@@ -39,5 +39,5 @@ def user(username):
                            chat_messages=messages)
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)
